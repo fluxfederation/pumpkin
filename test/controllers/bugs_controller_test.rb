46e8135f517859b6e607cb3fb14b8642606a3300
James Fargher <proglottis@gmail.com>
2016-09-15 15:51:49 +1200

    Happy bug schema verification

--- test/controllers/bugs_controller_test.rb
+++ test/controllers/bugs_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class BugsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include ActiveModelSerializers::Test::Schema
+
+  test "index" do
+    get bugs_path
+    assert_response :success
+    assert_response_schema "bugs/index.json"
+  end
 end
