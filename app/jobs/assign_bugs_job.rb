ad741c81853956a09cb3791ef48e030259a55d0f
James Fargher <proglottis@gmail.com>
2016-09-15 14:23:26 +1200

    Only look for matching bugs within patch

--- app/jobs/assign_bugs_job.rb
+++ app/jobs/assign_bugs_job.rb
@@ -8,7 +8,7 @@ class AssignBugsJob < ApplicationJob
   def perform(*args)
     occurrence = args[0]
     occurrence.with_lock do
-      occurrence.bug = Bug.joins(:primary_occurrence).merge(Occurrence.where(message: occurrence.message)).first
+      occurrence.bug = Bug.joins(:primary_occurrence).merge(Occurrence.where(patch_id: occurrence.patch, message: occurrence.message)).first
       if occurrence.bug.nil?
         occurrence.bug = Bug.create!(primary_occurrence: occurrence)
       end
