be19ca49d4690f223c0b8a30594b5a79ad18572d
James Fargher <proglottis@gmail.com>
2016-09-15 16:28:52 +1200

    More bug tests

--- test/controllers/bugs_controller_test.rb
+++ test/controllers/bugs_controller_test.rb
@@ -8,4 +8,10 @@ class BugsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_response_schema "bugs/index.json"
   end
+
+  test "show" do
+    get bug_path(bugs(:normal))
+    assert_response :success
+    assert_response_schema "bugs/show.json"
+  end
 end
