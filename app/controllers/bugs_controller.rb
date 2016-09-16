edeb03a2a55bfc7ad9b36449b30985b8bdf97509
James Fargher <proglottis@gmail.com>
2016-09-16 15:30:38 +1200

    More N+1

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,6 +1,6 @@
 class BugsController < ApplicationController
   def index
-    bugs = Bug.includes(:occurrences, :primary_occurrence)
+    bugs = Bug.includes({:occurrences => :patch}, {:primary_occurrence => :patch})
     bugs = bugs.where(:id => params[:ids]) if params[:ids]
     bugs = bugs.joins(:primary_occurrence).merge(Occurrence.where(:patch_id => params[:patch_ids])) if params[:patch_ids]
     render json: bugs, include: []
