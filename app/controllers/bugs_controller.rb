b1f380cc3156932f1bf4a4fda75c85f9d2990042
Michael Fowler <mkrfowler@gmail.com>
2016-10-21 10:50:21 +1300

    Viewify Bug#with_latest details
    Ordering is otherwise essentially the devil.

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,12 +1,12 @@
 class BugsController < ApplicationController
   def index
-    bugs = Bug.with_latest_details.includes({:occurrences => :patch}, {:primary_occurrence => :patch})
+    bugs = Bug.with_latest_details.order('last_occurred_at DESC').includes(:primary_occurrence => :patch)
     bugs = bugs.where(:id => params[:ids]) if params[:ids]
     bugs = bugs.joins(:primary_occurrence).merge(Occurrence.where(:patch_id => params[:patch_ids])) if params[:patch_ids]
     if params[:closed] == "true"
-      bugs = bugs.where("ev_latest.name = ?", "closed")
+      bugs = bugs.where("latest_event_name = ?", "closed")
     else
-      bugs = bugs.where("ev_latest.name <> ?", "closed")
+      bugs = bugs.where("latest_event_name <> ?", "closed")
     end
     render json: bugs, include: []
   end
--- app/models/bug.rb
+++ app/models/bug.rb
@@ -6,8 +6,6 @@ class Bug < ApplicationRecord
   belongs_to :latest_event, class_name: 'Event', optional: true
 
   def self.with_latest_details
-    select('DISTINCT ON (bugs.id) bugs.*, ev_latest.id AS latest_event_id, occ_latest.occurred_at AS last_occurred_at')
-    .from('bugs INNER JOIN occurrences occ_latest ON bugs.id = occ_latest.bug_id INNER JOIN events ev_latest ON bugs.id = ev_latest.bug_id')
-    .order('bugs.id, occ_latest.occurred_at DESC, ev_latest.created_at DESC')
+    from('bug_with_latest_details AS bugs')
   end
 end
