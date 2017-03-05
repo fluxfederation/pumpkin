7267ab17b3ee54275df1f7ba04af73e5a8863451
Ben Anderson <ben.anderson@powershop.co.nz>
2017-03-06 10:43:48 +1300

    Fix deploy configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,10 +4,11 @@ lock '3.6.1'
 set :application, 'com-test-pumpkin'
 
 set :pty, true
+set :shell, "/bin/bash"
 set :use_sudo, false
 set :scm, :git
 set :repo_url, 'git@git.powershop.co.nz:pumpkin/api.git'
-set :passenger_restart_with_sudo, true
+# set :passenger_restart_/with_sudo, true
 
 set :bundle_without, [:development, :test, :deployment]
 
@@ -18,7 +19,7 @@ set(:linked_dirs) { %w(log) }
 set :rails_env, 'production'
 
 before "bundler:install", "deploy:fix_permissions"
-before  "deploy:migrate", "deploy:link_global_app_env"
+before "deploy:migrate",  "deploy:link_global_app_env"
 
 namespace :deploy do
   task :fix_permissions do
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'com-test-pumpkin-ccp1', roles: %w{app db web}
+server 'com-test-pumpkin-ccp2', roles: %w{app db web}
 
 
 # role-based syntax
