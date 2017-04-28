c3d535a19ecff2ab4e0214883a58400d3d765094
Steve Purcell <steve@sanityinc.com>
2017-04-28 13:26:07 +1200

    Emacs directory-locals for Elm source

---
+++ elm/.dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((elm-mode
+  (elm-format-on-save . t)
+  (elm-main-file . "src/Main.elm")
+  (flycheck-elm-main-file . "src/Main.elm")
+  (flycheck-elm-output-file . "../public/js/app.js")))
