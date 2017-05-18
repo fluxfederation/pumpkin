27ffa00e7c4f19a1124e405cd16af67c0e360794
Andy Newport <andy.newport@powershop.co.nz>
2017-05-18 15:29:55 +1200

    Block invalid environment names

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -1,4 +1,15 @@
 class Environment < ApplicationRecord
   has_many :occurrences, dependent: :destroy
   has_many :bugs, through: :occurrences
+
+  # Lifted (and extended) from Mr. Wippy, our WIP environment deployer
+  validates :id,
+    presence: true,
+    length: { maximum: 30 }
+  validates :id,
+    format: { without: /\A[\-\.]/, message: "cannot start with a hyphen or dot" }
+  validates :id,
+    format: { without: /[\-\.]\z/, message: "cannot end with a hyphen or dot" }
+  validates :id,
+    format: { with: /\A[a-zA-Z0-9\.\-]+\z/, message: "must only contain letters, numbers, hyphens or periods" }
 end
--- test/models/environment_test.rb
+++ test/models/environment_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class EnvironmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "it doesn't allow invalid env names" do
+    env = Environment.new(id: "va-1-id-199")
+    assert(env.valid?)
+
+    # No blank names
+    env.id = nil
+    assert_not(env.valid?)
+
+    env.id = ""
+    assert_not(env.valid?)
+
+    # No start hyphens
+    env.id = "-invalid"
+    assert_not(env.valid?)
+
+    # No start periods
+    env.id = ".invalid"
+    assert_not(env.valid?)
+
+    # No end hyphens
+    env.id = "invalid-"
+    assert_not(env.valid?)
+
+    # No end periods
+    env.id = "invalid."
+    assert_not(env.valid?)
+
+    #No odd chars
+    env.id = "inva|id"
+    assert_not(env.valid?)
+  end
 end
