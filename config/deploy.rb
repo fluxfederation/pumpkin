43206713b3293838dd92c27e26f2e4cba99ba0ec
Krasimir Angelov <krasio@codingspree.net>
2017-05-24 21:59:05 +1200

    Stop/start Resque workers on deploy.
    ATM if you are not Ben you need to sudo on both servers first. :/

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,9 @@ set(:linked_dirs) { %w(log tmp/cache) }
 set :rails_env, 'production'
 
 before "bundler:install", "deploy:fix_permissions"
-before "deploy:assets:precompile",  "deploy:link_global_app_env"
+before "deploy:assets:precompile", "deploy:link_global_app_env"
+before "deploy:migrate", "workers:stop"
+before "passenger:restart", "workers:start"
 
 namespace :deploy do
   task :fix_permissions do
@@ -32,6 +34,22 @@ namespace :deploy do
   task :link_global_app_env do
     on roles(:app) do
       execute "ln -nfs /etc/app.env #{release_path}/.env"
+    end
+  end
+end
+
+namespace :workers do
+  desc "Stop Resque workers"
+  task :stop do
+    on roles(:app) do
+      execute :sudo, "service resque stop"
+    end
+  end
+
+  desc "Start Resque workers"
+  task :start do
+    on roles(:app) do
+      execute :sudo, "service resque start"
     end
   end
 end
