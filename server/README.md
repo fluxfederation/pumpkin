0ec9a82b6207a4215d193a80155252efd4cc7568
Steve Purcell <steve@sanityinc.com>
2017-06-27 21:11:08 +1200

    Parse command-line args with optparse-applicative

--- server/README.md
+++ server/README.md
@@ -6,7 +6,7 @@ For local development:
 ```
 stack build
 export PGDATABASE=pumpkin_development
-stack exec pumpkin-server ../public 8080 8000
+stack exec -- pumpkin-server --rootdir ../public
 ```
 
 API requests will then be served on 8080, while EKG metrics will be visible at port 8000.
--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,13 +1,33 @@
 module Main where
 
 import Server
-import System.Environment (getArgs)
-import System.Exit (die)
+
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data Opts = Opts
+  { publicDir :: FilePath
+  , serverPort :: Int
+  , ekgPort :: Int
+  }
+
+parseOpts :: Parser Opts
+parseOpts =
+  Opts <$> strOption (long "rootdir" <> help "Web root dir") <*>
+  option
+    auto
+    (long "port" <> showDefault <> value 8080 <> help "Web server port") <*>
+  option
+    auto
+    (long "ekgport" <> showDefault <> value 8000 <> help "EKG front end port")
+
+run :: Opts -> IO ()
+run opts = runServer (publicDir opts) (serverPort opts) (ekgPort opts)
 
 main :: IO ()
-main = do
-  args <- getArgs
-  if length args /= 3
-    then die "usage: pumpkin-server rootdir serverport ekgport"
-    else let [publicdir, serverPort, ekgPort] = args
-         in runServer publicdir (read serverPort) (read ekgPort)
+main = run =<< execParser opts
+  where
+    opts =
+      info
+        (parseOpts <**> helper)
+        (fullDesc <> progDesc "Serve the pumpkin API")
